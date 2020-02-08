45868125bb3b39e4f7923a5f8303fedcb3406725
Randon <randon.shaw@gmail.com>
2020-02-07 18:38:24 -0700

    set up side drawer functionality and animation

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
 
 import './SideDrawer.css'
 
-// drawer-hook ID is located above the root ID in index.html. It's displayed as before, but rendered differently and semantically better. Portals allow to render content of component in a different place than the component itself. 
+// drawer-hook ID is located above the root ID in index.html. It's displayed as before, but rendered differently and semantically better. Portals allow to render content of component in a different place than the component itself.
 const SideDrawer = props => {
-  const content = <aside className="side-drawer">{props.children}</aside>
+	const content = (
+		<CSSTransition
+			in={props.show}
+			timout={200}
+			classNames="slide-in-left"
+			mountOnEnter
+			unmountOnExit
+		>
+			<aside className="side-drawer" onClick={props.onClick}>
+				{props.children}
+			</aside>
+		</CSSTransition>
+	)
 
-  return ReactDOM.createPortal(content, document.getElementById('drawer-hook')) 
+	return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
 }
 
 export default SideDrawer
